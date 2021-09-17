58ed81f12d3ce3b47d0bd2df8a78db5552f4ca47
Karol Ciołczyk <dyweko@gmail.com>
2021-09-17 14:31:04 +0200

    userSessionContex created, Router added in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import "./App.css";
+import { UserSessionContext } from "./context/UserSesionContext";
 
 import { Navbar } from "./components/Navbar";
 import { Card } from "./components/Card";
 import { Loginform } from "./components/LoginForm";
 import { SignUpForm } from "./components/SignUpForm";
-import { TodoItem } from "./components/TodoItem";
+// import { TodoItem } from "./components/TodoItem";
 // import { TodoList } from "./components/TodoList";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Card>
-        <Loginform />
-      </Card>
-      <Card>
-        <SignUpForm />
-      </Card>
-      {/* <TodoList /> */}
-    </div>
+    <UserSessionContext.Provider
+      value={{
+        token: "",
+      }}
+    >
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Card>
+            <Loginform />
+          </Card>
+          <Card>
+            <SignUpForm />
+          </Card>
+          {/* <TodoList /> */}
+        </div>
+      </Router>
+    </UserSessionContext.Provider>
   );
 }
 
