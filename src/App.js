1b74c5d48e77d18c1865f9cec301315b1f857758
Karol Ciołczyk <dyweko@gmail.com>
2021-09-17 14:30:55 +0200

    userSessionContex created, Router added in App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import "./App.css";
 
 import { Navbar } from "./components/Navbar";
-// import { Card } from "./components/Card";
-// import { Loginform } from "./components/LoginForm";
-// import { SignUpForm } from "./components/SignUpForm";
-// import { TodoItem } from "./components/TodoItem";
-// import { TodoTasks } from "./components/TodoTasks";
-import { TodoList } from "./components/TodoList";
+import { Card } from "./components/Card";
+import { Loginform } from "./components/LoginForm";
+import { SignUpForm } from "./components/SignUpForm";
+import { TodoItem } from "./components/TodoItem";
+// import { TodoList } from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <TodoItem />
       <Card>
         <Loginform />
       </Card>
       <Card>
         <SignUpForm />
       </Card>
-      <TodoTasks /> */}
-      <TodoList />
+      {/* <TodoList /> */}
     </div>
   );
 }
